e04523416fb87d66bbe7284218a097b6e6c1531c
mausami-dangi <mausamidangi@gmail.com>
2025-10-30 15:09:43 +0530

    Initial commit

---
+++ WingUI/WingUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WingUIApp.swift
+//  WingUI
+//
+//  Created by Apple on 30/10/25.
+//
+
+import SwiftUI
+
+@main
+struct WingUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
